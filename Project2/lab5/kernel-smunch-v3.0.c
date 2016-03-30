78a2a5ea21b0fdc7c7c8ca2bfa36858c34d9520c
Yitian <yitianl@g.clemson.edu>
2016-03-30 19:02:55 -0400

    Project3 begin

---
+++ Project2/lab5/kernel-smunch-v3.0.c
@@ -0,0 +1,41 @@
+#include <linux/linkage.h>
+#include <linux/signal.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+
+SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
+{
+	
+	struct task_struct *p;
+	sigset_t signal;
+	signal.sig[0]=bit_pattern;
+
+	
+	p = find_task_by_vpid(pid);
+	if (!p)
+	{
+		printk(KERN_ALERT "Find Process PID failed!\n");
+		return (-1);
+	}
+	
+	if (thread_group_empty(p)!=1 || (p->ptrace & PT_SEIZED))
+	{
+		printk(KERN_ALERT "This is not Single or Traced!\n");
+		return (-1);
+	}
+
+	//check if there is SIGKILL and is ZOMBIE:
+	if (sigismember(&signal,SIGKILL)==1 && p->exit_state==EXIT_ZOMBIE)
+	{
+			release_task(p);
+			return (0);
+	}
+	
+	p->pending.signal=signal;
+	wake_up_process(p);
+	//Check if it is in deepsleep,if in wake it up
+	
+	return (0);
+}
